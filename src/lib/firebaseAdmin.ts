a8fa25efb381f80eea72f1372ff549a4c3ba8629
iaidrew <adityaofficial9918@gmail.com>
2026-03-06 18:46:37 +0530

    fix netlify publish directory

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -2,7 +2,7 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-console.log("ENV CHECK:", process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
 
 const serviceAccountRaw = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
 
